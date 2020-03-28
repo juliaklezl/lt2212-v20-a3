f8c509c6948d5acffeccba6201d963744db962d4
Julia Klezl <gusklezju@GU.GU.SE@mltgpu.flov.gu.se>
2020-03-28 19:01:51 +0100

    part 1 and 2 (prel.)

--- a3_features.py
+++ a3_features.py
@@ -4,6 +4,10 @@ import argparse
 import numpy as np
 import pandas as pd
 # Whatever other imports you need
+from glob import glob
+from  sklearn.decomposition import PCA
+from random import choices
+import csv
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert directories into table.")
@@ -16,12 +20,56 @@ if __name__ == "__main__":
 
     print("Reading {}...".format(args.inputdir))
     # Do what you need to read the documents here.
-
+    directories = glob("{}/*".format(args.inputdir))
+    rows = [] #initiate list of dictionaries (one per news post)
+    index = 0 # initiate index (of words)
+    index_dict = {} # initiate dict to keep track of words and indexes
+    author_list = [] # initiate list for author labels
     print("Constructing table with {} feature dimensions and {}% test instances...".format(args.dims, args.testsize))
     # Build the table here.
-    
+    for directory in directories:   
+        author = directory.split("/")[-1]
+        filenames = glob("{}/*".format(directory))
+        for file in filenames:
+            row_counts = {} # initiate dict to keep track of words and indexes
+            author_list.append(author)
+            with open(file, "r") as the_file:
+                wholetext = the_file.readlines()
+                for  line in wholetext:
+                    for word in line.split():
+                        if word.isalpha():
+                            word = word.lower() # lowercase
+                            if word in index_dict:
+                                i = index_dict[word] # get index, if already assigned,
+                            else:
+                                index_dict[word] = index # otherwise assign index
+                                i = index
+                                index += 1 # and raise index counter by one
+                            if i in row_counts: # get word counts per text
+                                row_counts[i] += 1
+                            else:
+                                row_counts[i] = 1
+            rows.append(row_counts)
+    features = np.zeros((len(rows), len(index_dict))) # instantiate empty ndarray
+    for n, dict in enumerate(rows):
+        for ind, val in dict.items():
+            features[n, ind] = val # replace zeros by counts
+    pca = PCA(n_components = args.dims)
+    red_features = pca.fit_transform(features)
+    author_array = np.array([author_list])
+    red_features = np.concatenate((author_array.T, red_features), axis=1)
+    test = args.testsize/100
+    train = 1-test
+    train_test_list = choices(["train", "test"], [train, test], k = len(author_list))
+    tt_array = np.array([train_test_list])
+    finished_features = np.concatenate((tt_array.T, red_features), axis = 1)
+    # write to csv
     print("Writing to {}...".format(args.outputfile))
+    with open(args.outputfile, 'w+') as output:
+        csvWriter = csv.writer(output, delimiter=',')
+        csvWriter.writerows(finished_features)
+#    np.savetxt(output, finished_features, delimiter=',')
     # Write the table out here.
-
+    print(finished_features)
     print("Done!")
     
--- a3_model.py
+++ a3_model.py
@@ -6,13 +6,92 @@ import pandas as pd
 import torch
 from torch import nn
 from torch import optim
+import random
+from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score
 # Whatever other imports you need
 
 # You can implement classes and helper functions here too.
 
+class AuthorPredict(nn.Module):
+    def __init__(self, input_size, output_size=1):
+        super().__init__()
+        self.linear = nn.Linear(input_size, output_size)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        m = self.linear(x)
+        n = self.sigmoid(m)
+        return n
+
+class AuthorFFNN:
+    def __init__(self, lr =0.01):
+        self.lr = lr
+    def train(self, inputs, samplesize): 
+        self.model = AuthorPredict((inputs.shape[1]-2)*2)
+        criterion = nn.BCELoss()
+        optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
+
+        for i in range(samplesize):
+            documents = inputs.drop(inputs.columns[[0,1]], axis=1)
+            labels = inputs.iloc[:, 1:2]
+            in1 = random.randint(0, len(documents))
+            auth = labels.iloc[in1, 0]
+            in_same = labels[labels.iloc[:, 0] == auth].index
+            in_diff = labels[labels.iloc[:, 0] != auth].index
+            coin = random.randint(0, 1)
+            if coin == 0:
+                in2 = random.choice(in_diff)
+            if coin == 1:
+                in2 = random.choice(in_same)
+            docs = documents.to_numpy()
+            doc1 = torch.Tensor(docs[in1])
+            doc2 = torch.Tensor(docs[in2])
+            instance = torch.cat((doc1, doc2), 0)
+            output = self.model(instance)
+            label = torch.Tensor([coin])
+            loss = criterion(output, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    def test(self, inputs, samplesize):
+        pred_list = []
+        label_list = []
+        for i in range(samplesize):
+            documents = inputs.drop(inputs.columns[[0,1]], axis=1)
+            labels = inputs.iloc[:, 1:2]
+            in1 = random.randint(0, len(documents))
+            auth = labels.iloc[in1, 0]
+            in_same = labels[labels.iloc[:, 0] == auth].index
+            in_diff = labels[labels.iloc[:, 0] != auth].index
+            coin = random.randint(0, 1)
+            if coin == 0:
+                in2 = random.choice(in_diff)
+            if coin == 1:
+                in2 = random.choice(in_same)
+            label_list.append(coin)
+            docs = documents.to_numpy()
+            doc1 = torch.Tensor(docs[in1])
+            doc2 = torch.Tensor(docs[in2])
+            instance = torch.cat((doc1, doc2), 0)
+
+            output = self.model(instance)
+            if output > 0.5:
+                predict = 1
+            else:
+                predict = 0
+            pred_list.append(predict)
+        print(label_list)
+
+        accuracy = accuracy_score(label_list, pred_list)
+        f = f1_score(label_list, pred_list, average='weighted')
+        recall = recall_score(label_list, pred_list, average='weighted')
+        precision = precision_score(label_list, pred_list, average='weighted')
+        print('accuracy:', accuracy, 'precision:', precision, 'recall:', recall, 'f1_score:', f)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train and test a model on features.")
     parser.add_argument("featurefile", type=str, help="The file containing the table of instances and features.")
+    parser.add_argument("samplesize", type = int, help="The number of text pairs used to train the FFNN.")
     # Add options here for part 3 -- hidden layer and nonlinearity,
     # and any other options you may think you want/need.  Document
     # everything.
@@ -20,6 +99,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print("Reading {}...".format(args.featurefile))
+    data = pd.read_csv(args.featurefile, header = None)
+    train = data[data[0] == "train"]
+    train.reset_index(inplace=True, drop=True)
+    test = data[data[0] == "test"]
+    test.reset_index(inplace=True, drop=True)
+
+    ffnn = AuthorFFNN()
+    ffnn.train(train, args.samplesize)
+    ffnn.test(test, args.samplesize)
 
     # implement everything you need here
     
+
